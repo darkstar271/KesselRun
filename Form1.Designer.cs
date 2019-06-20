67ad2f180b511085bdbd3d94146486f61ba0c84e
Darren Gorman <saintred-tech@hotmail.com>
2019-06-20 13:56:16 +1200

    ver3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,78 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.Pb1 = new System.Windows.Forms.PictureBox();
+            this.Pb2 = new System.Windows.Forms.PictureBox();
+            this.Pb3 = new System.Windows.Forms.PictureBox();
+            this.Pb4 = new System.Windows.Forms.PictureBox();
+            this.UdBet = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.btnStart = new System.Windows.Forms.Button();
             this.btnReset = new System.Windows.Forms.Button();
             this.btnWager = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
-            this.radioButton4 = new System.Windows.Forms.RadioButton();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.RbWatto = new System.Windows.Forms.RadioButton();
+            this.RbSnoke = new System.Windows.Forms.RadioButton();
+            this.RbJubba = new System.Windows.Forms.RadioButton();
+            this.RbCreedo = new System.Windows.Forms.RadioButton();
+            this.lblWagerName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UdBet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // Pb1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.BackgroundImage = global::KesselRun.Resource1.Millenium_Falcon_920x518;
-            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(101, 79);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.Pb1.BackColor = System.Drawing.Color.Transparent;
+            this.Pb1.BackgroundImage = global::KesselRun.Resource1.Millenium_Falcon_920x518;
+            this.Pb1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Pb1.Location = new System.Drawing.Point(12, 12);
+            this.Pb1.Name = "Pb1";
+            this.Pb1.Size = new System.Drawing.Size(101, 79);
+            this.Pb1.TabIndex = 0;
+            this.Pb1.TabStop = false;
             // 
-            // pictureBox2
+            // Pb2
             // 
-            this.pictureBox2.BackgroundImage = global::KesselRun.Resource1.Scimitar_BF2;
-            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(12, 106);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(101, 79);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.Pb2.BackgroundImage = global::KesselRun.Resource1.Scimitar_BF2;
+            this.Pb2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Pb2.Location = new System.Drawing.Point(12, 106);
+            this.Pb2.Name = "Pb2";
+            this.Pb2.Size = new System.Drawing.Size(101, 79);
+            this.Pb2.TabIndex = 1;
+            this.Pb2.TabStop = false;
             // 
-            // pictureBox3
+            // Pb3
             // 
-            this.pictureBox3.BackgroundImage = global::KesselRun.Resource1.Slave_1;
-            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox3.Location = new System.Drawing.Point(12, 203);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(101, 79);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.Pb3.BackgroundImage = global::KesselRun.Resource1.Slave_1;
+            this.Pb3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Pb3.Location = new System.Drawing.Point(12, 203);
+            this.Pb3.Name = "Pb3";
+            this.Pb3.Size = new System.Drawing.Size(101, 79);
+            this.Pb3.TabIndex = 2;
+            this.Pb3.TabStop = false;
             // 
-            // pictureBox4
+            // Pb4
             // 
-            this.pictureBox4.BackgroundImage = global::KesselRun.Resource1._68_960x600f;
-            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox4.Location = new System.Drawing.Point(12, 320);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(101, 79);
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
+            this.Pb4.BackgroundImage = global::KesselRun.Resource1._68_960x600f;
+            this.Pb4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Pb4.Location = new System.Drawing.Point(12, 320);
+            this.Pb4.Name = "Pb4";
+            this.Pb4.Size = new System.Drawing.Size(101, 79);
+            this.Pb4.TabIndex = 3;
+            this.Pb4.TabStop = false;
             // 
-            // numericUpDown1
+            // UdBet
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(623, 463);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
-            this.numericUpDown1.TabIndex = 4;
+            this.UdBet.Location = new System.Drawing.Point(833, 345);
+            this.UdBet.Name = "UdBet";
+            this.UdBet.Size = new System.Drawing.Size(120, 20);
+            this.UdBet.TabIndex = 4;
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(623, 517);
+            this.numericUpDown2.Location = new System.Drawing.Point(833, 399);
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
             this.numericUpDown2.TabIndex = 5;
@@ -134,49 +135,58 @@
             this.btnWager.UseVisualStyleBackColor = true;
             this.btnWager.Click += new System.EventHandler(this.BtnStart_Click);
             // 
-            // radioButton1
+            // RbWatto
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(409, 449);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(85, 17);
-            this.radioButton1.TabIndex = 9;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "radioButton1";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.RbWatto.AutoSize = true;
+            this.RbWatto.Location = new System.Drawing.Point(397, 469);
+            this.RbWatto.Name = "RbWatto";
+            this.RbWatto.Size = new System.Drawing.Size(54, 17);
+            this.RbWatto.TabIndex = 9;
+            this.RbWatto.TabStop = true;
+            this.RbWatto.Text = "Watto";
+            this.RbWatto.UseVisualStyleBackColor = true;
+            this.RbWatto.CheckedChanged += new System.EventHandler(this.RbWatto_CheckedChanged);
             // 
-            // radioButton2
+            // RbSnoke
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(409, 491);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(85, 17);
-            this.radioButton2.TabIndex = 10;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "radioButton2";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.RbSnoke.AutoSize = true;
+            this.RbSnoke.Location = new System.Drawing.Point(395, 423);
+            this.RbSnoke.Name = "RbSnoke";
+            this.RbSnoke.Size = new System.Drawing.Size(56, 17);
+            this.RbSnoke.TabIndex = 10;
+            this.RbSnoke.TabStop = true;
+            this.RbSnoke.Text = "Snoke";
+            this.RbSnoke.UseVisualStyleBackColor = true;
             // 
-            // radioButton3
+            // RbJubba
             // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(409, 539);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(85, 17);
-            this.radioButton3.TabIndex = 11;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "radioButton3";
-            this.radioButton3.UseVisualStyleBackColor = true;
+            this.RbJubba.AutoSize = true;
+            this.RbJubba.Location = new System.Drawing.Point(395, 382);
+            this.RbJubba.Name = "RbJubba";
+            this.RbJubba.Size = new System.Drawing.Size(54, 17);
+            this.RbJubba.TabIndex = 11;
+            this.RbJubba.TabStop = true;
+            this.RbJubba.Text = "Jubba";
+            this.RbJubba.UseVisualStyleBackColor = true;
             // 
-            // radioButton4
+            // RbCreedo
             // 
-            this.radioButton4.AutoSize = true;
-            this.radioButton4.Location = new System.Drawing.Point(409, 562);
-            this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(85, 17);
-            this.radioButton4.TabIndex = 12;
-            this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "radioButton4";
-            this.radioButton4.UseVisualStyleBackColor = true;
+            this.RbCreedo.AutoSize = true;
+            this.RbCreedo.Location = new System.Drawing.Point(392, 345);
+            this.RbCreedo.Name = "RbCreedo";
+            this.RbCreedo.Size = new System.Drawing.Size(59, 17);
+            this.RbCreedo.TabIndex = 12;
+            this.RbCreedo.TabStop = true;
+            this.RbCreedo.Text = "Creedo";
+            this.RbCreedo.UseVisualStyleBackColor = true;
+            // 
+            // lblWagerName
+            // 
+            this.lblWagerName.Location = new System.Drawing.Point(466, 345);
+            this.lblWagerName.Name = "lblWagerName";
+            this.lblWagerName.Size = new System.Drawing.Size(238, 141);
+            this.lblWagerName.TabIndex = 13;
+            this.lblWagerName.Text = "label1";
             // 
             // Form1
             // 
@@ -185,26 +195,27 @@
             this.BackgroundImage = global::KesselRun.Resource1.starry_sky_night_stars_115042_1920x1080;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1318, 617);
-            this.Controls.Add(this.radioButton4);
-            this.Controls.Add(this.radioButton3);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.lblWagerName);
+            this.Controls.Add(this.RbCreedo);
+            this.Controls.Add(this.RbJubba);
+            this.Controls.Add(this.RbSnoke);
+            this.Controls.Add(this.RbWatto);
             this.Controls.Add(this.btnWager);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.numericUpDown2);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.UdBet);
+            this.Controls.Add(this.Pb4);
+            this.Controls.Add(this.Pb3);
+            this.Controls.Add(this.Pb2);
+            this.Controls.Add(this.Pb1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UdBet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -213,19 +224,20 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox Pb1;
+        private System.Windows.Forms.PictureBox Pb2;
+        private System.Windows.Forms.PictureBox Pb3;
+        private System.Windows.Forms.PictureBox Pb4;
+        private System.Windows.Forms.NumericUpDown UdBet;
         private System.Windows.Forms.NumericUpDown numericUpDown2;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Button btnReset;
         private System.Windows.Forms.Button btnWager;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton RbWatto;
+        private System.Windows.Forms.RadioButton RbSnoke;
+        private System.Windows.Forms.RadioButton RbJubba;
+        private System.Windows.Forms.RadioButton RbCreedo;
+        private System.Windows.Forms.Label lblWagerName;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,23 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using KesselRun.Business;
+using KesselRun.Business.AllPunters;
 
 namespace KesselRun
 {
     public partial class Form1 : Form
     {
+        // Create my Ships
+        Ships[] ships = new Ships[4];
+        Punter[] myPunter = new Punter[4];
+        Punter CurrantPunter = new Snoke();
         public Form1()
         {
             InitializeComponent();
+            LoadPunters();
+            LoadShips();
         }
 
+        #region Radio & Button Switch
+        // this is a fake button and switch for all buttons
         private void BtnStart_Click(object sender, EventArgs e)
         {
-            Button fakeButton = (Button) sender;
+            Button fakeButton = (Button)sender;
             ButtonSwitch(fakeButton.Name);
         }
-
-
         public void ButtonSwitch(string ButNum)
 
         {
@@ -31,25 +39,108 @@ namespace KesselRun
                 switch (ButNum)
                 {
                     case "btnStart":
-
+                        // HyperSpace();
                         break;
                     case "btnReset":
+                        ResetAll();
                         break;
                     case "btnWager":
+
+                        Wager();
                         break;
                     case "4":
                         break;
                 }
-
-
-
-
-
             }
+        }
+
+        // this is a fake radio button and switch for all radio buttons
+        private void RbWatto_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton fakeRb = new RadioButton();
+            fakeRb = (RadioButton)sender;
+
+
+            if (fakeRb.Checked == true)
+
+            {
+                // look for the name of the person we have clicked on
+                this.Name = fakeRb.Name;
+
+                // pass the data across to currentPunter.
+                switch (fakeRb.Name)
+                {
+
+                    case "RbCreedo":
+                        CurrantPunter = myPunter[0];
+                        break;
+
+
+                    case "RbJubba":
+                        CurrantPunter = myPunter[1];
+                        break;
+
+
+                    case "RbSnoke":
+                        CurrantPunter = myPunter[2];
+                        break;
+                    case "RbWatto":
+                        CurrantPunter = myPunter[3];
+                        break;
+
+                }
+
+                this.Name = CurrantPunter.PunterName + " is the currant Punter";
+            }
+
+
+
+            UdBet.Maximum = (decimal)CurrantPunter.Cash;
 
 
 
         }
 
+        #endregion
+
+        private void Wager()
+        {
+            CurrantPunter.Bet = (float)UdBet.Value;
+            lblWagerName.Text += CurrantPunter.PunterName + " Bets" + CurrantPunter.Bet + Environment.NewLine;
+        }
+
+        private void ResetAll()
+
+        {
+            this.Text = "";
+            // resets the left to 10 for the race 
+            for (int i = 0; i < 4; i++)
+            {
+                ships[i].myPB.Left = 10;
+            }
+
+
+        }
+
+        private void LoadPunters()
+
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                myPunter[i] = Factory.GetAPunter(i);
+                //   myPunter[i].LabelWinner =  lblWinner;
+            }
+
+
+        }
+
+        private void LoadShips()
+        {
+            ships[0] = new Ships { Lenght = 0, myPB = Pb1, Name = "Falcon" };
+            // monster[0].myPB.BackgroundImage = Resource1.Agor;
+            ships[1] = new Ships { Lenght = 0, myPB = Pb2, Name = "Scimitar" };
+            ships[2] = new Ships { Lenght = 0, myPB = Pb3, Name = "Slave 1" };
+            ships[3] = new Ships { Lenght = 0, myPB = Pb4, Name = "Rage" };
+        }
     }
 }
